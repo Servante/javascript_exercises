0808a6545a96135614b7e49d6b59880ad38aca46
Wesley <matthew282@msn.com>
2022-11-29 10:38:41 -0700

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return "ERROR"
+  } else {
+    let result = []
+    while (num > 0) {
+      result.push(str);
+      num--;
+    }
+    return result.join('');
+  }  
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...v) {
+  index = arr.indexOf(v);
+  arr.splice(index, 1);
+  return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// if Array.isArray(v)
+//   loop through array and find indexes
+//   loop through given array removing elements
+// else
+// standard method of removing element (see if the above looping would work with just one element too)
