852d811efcd03da6b5f160af15e0a9500bc4ca98
Wesley <matthew282@msn.com>
2023-01-16 13:35:48 -0700

    add guard clause and array numerical sort to sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) { 
+  let array = [];
+  if (x > 0 && y > 0 && typeof(x) != 'string' && typeof(y) != 'string') {
+    array.push(x, y);
+    array.sort((a, b) => a - b);
+    
+  } else {
+    'ERROR'
+  }
 };
 
 // Do not edit below this line
