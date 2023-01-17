c277819e33d3ac3eec0d8ef741c79dfedc707f76
Wesley <matthew282@msn.com>
2023-01-17 16:05:40 -0700

    create function and all tests passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,19 @@
 const sumAll = function(x, y) { 
   let array = [];
-  if (x > 0 && y > 0 && typeof(x) != 'string' && typeof(y) != 'string') {
+  if (x > 0 && y > 0 && typeof(x) == 'number' && typeof(y) == 'number') {
     array.push(x, y);
     array.sort((a, b) => a - b);
-    
+    const start = array[0];
+    const target = array[1];
+    var sum = start;
+    var next = start + 1;
+    for (i = start; i < target; i++){
+      sum += next;
+      next++;
+    }
+    return sum;
   } else {
-    'ERROR'
+    return 'ERROR';
   }
 };
 
