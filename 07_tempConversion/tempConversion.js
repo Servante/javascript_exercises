bbedf3a7c868ea2769ba271bba363f0af998d190
Wesley <matthew282@msn.com>
2023-01-18 17:35:34 -0700

    create convertToCelsius and convertToFahrenheit / all tests passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  var c = f - 32;
+      c = c * 5;
+      c = c / 9;
+      return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  var f = c * 1.8;
+      f = f + 32;
+      return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
