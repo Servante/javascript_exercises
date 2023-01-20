f031262e6c20edbdd61f1985f74b9412bd012376
Wesley <matthew282@msn.com>
2023-01-20 16:41:21 -0700

    complete functions add(), subtract(), sum(), multiply(), power(), factorial(). all tests passing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,52 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0 ) {
+    return 0;
+  } else {
+    var result = arr[0];
+    for (i = 1; i < arr.length; i++) {result += arr[i]}
+    return result;
+  } 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  var result = arr[0];
+  for (i = 1; i < arr.length; i++) { result = result * arr[i]}
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1){
+    return 1;
+  } else {
+    for (var i = num - 1; i >= 1; i-- ){num *= i }
+    return num;  
+  }
 };
+
+/*
+
+if (num > 5 || num == 7) {
+    console.log("test1");
+} else {
+    console.log("test2");
+};
+
+OR works funny, try using it in reverse
+
+*/
+
 
 // Do not edit below this line
 module.exports = {
