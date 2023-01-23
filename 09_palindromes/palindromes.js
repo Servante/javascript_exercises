7e9760a9feab3184396672d3b2b3138fa26cee1f
Wesley <matthew282@msn.com>
2023-01-23 16:41:36 -0700

    create palindromes() and all tests passing

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let regex = /[A-Z]/gi;
+  let string = str.toLowerCase().match(regex).join(''); 
+  let revStrn =  string.split('').reverse().join('');
+  return revStrn == string ? true : false;
 };
 
 // Do not edit below this line
